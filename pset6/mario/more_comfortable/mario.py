230a32eeafcc55c366546419ea3d7f07ad2020ee
allapetr <69552018+allapetr@users.noreply.github.com>
2022-09-07 00:04:03 +0300

    Create mario.py

---
+++ pset6/mario/more_comfortable/mario.py
@@ -0,0 +1,17 @@
+from cs50 import get_int
+
+# Prompt user for height
+n = 0
+while n < 1 or n > 8:
+    n = get_int("Height: ")
+
+# Build piramid
+for x in range(n):
+    for z in range(n - (x + 1)):
+        print(" ", end="")
+    for y in range(x + 1):
+        print("#", end="")
+    print("  ", end="")
+    for y in range(x + 1):
+        print("#", end="")
+    print()
